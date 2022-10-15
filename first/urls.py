0efd1b52399814777323f3a5c1c66804fbc1a6bc
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-10-15 17:10:54 +0530

    Update urls.py

--- first/urls.py
+++ first/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('base/',views.base,name='base'),
     path('home/',views.home,name='home'),
     path('task/',views.task,name='task'),
+       path('submit/',views.submit,name='submit'),
      path('subtask/',views.subtask,name='subtask'),
     path('subtasks/',views.subtasks,name='subtasks'),
     path('sign-up/',views.sign_up,name='sign_up'),
