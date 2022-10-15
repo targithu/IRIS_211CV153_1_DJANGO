e6f72a5462d0bc84855bf56474cb632a308ba38f
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-10-15 20:23:27 +0530

    Update settings.py

--- task1/settings.py
+++ task1/settings.py
@@ -121,5 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL='/case'
+LOGIN_REDIRECT_URL='/none'
 LOGOUT_REDIRECT_URL='/login'
